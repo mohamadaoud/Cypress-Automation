7d137d91e80b5f7cbb49309f3bb744597a995d0b
mohamadaoud <daoud_mohamad@yahoo.co.uk>
2022-04-24 16:47:38 +0200

    commit changes

--- cypress/integration/login-spec.js
+++ cypress/integration/login-spec.js
@@ -1,13 +1,16 @@
 describe('Cypress Automation Demo', () => {
     beforeEach(() => {
+      //open baseUrl before each test we run
       cy.visit(Cypress.env('baseUrl'))
       // we are not logged in
     })
 
     it('Login Scenario with wrong credentials', () => {
-    
+        //select CSS selector for username field and type 'wrong@email.com'
         cy.get('#username').type('wrong@email.com')
+        //select CSS selector for password field and type 'no-such-user'
         cy.get('#password').type('no-such-user')
+        //select CSS selector for login button and click it'
         cy.get('#login > button').click()   
     
         // error message is shown and we remain on the login page
@@ -18,9 +21,14 @@ describe('Cypress Automation Demo', () => {
   
     it('Login Scenario with correct credentials', () => {
         const user = Cypress.env('user')
+        //select CSS selector for username field
+        //bring email value from cypress.json file using user variable
         cy.get('#username').type(user.email)
+        //select CSS selector for password field
+        //bring password value from cypress.json file using user variable
         cy.get('#password').type(user.password)
-        cy.get('#login > button > i').click() 
+        //select CSS selector for login button and click it'
+        cy.get('#login > button').click() 
         // url is /
         cy.url().should('not.contain', '/login')      
     })
@@ -31,6 +39,7 @@ describe('Cypress Automation Demo', () => {
         cy.get('#username').type(user.email)
         cy.get('#password').type(user.password)
         cy.get('#login > button > i').click() 
+        //select CSS selector for logout button and click it'
         cy.get('#content > div > a > i').click()
 
         //Logged out message is shown when we redirected to the login page
